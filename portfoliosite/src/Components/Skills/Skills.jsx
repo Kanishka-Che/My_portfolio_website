2fd7365f4dc4a192039298e4607c5d0db7538520
Kanishka-Che <akcadikari@std.appsc.sab.ac.lk>
2024-10-02 12:29:02 +0530

    update04

--- portfoliosite/src/Components/Skills/Skills.jsx
+++ portfoliosite/src/Components/Skills/Skills.jsx
@@ -14,25 +14,20 @@ const Skills = () => {
   return (
     <section className='skills-container'>
       <h2>Technical Proficiency</h2>
+
       <div className='skills-content'>
         <div className='skills'>
           <div>
             {SKILLS.map((item) => (
-              <div key={item.title}>
+              
                 <SkillCard
+                  key={item.title}
                   iconUrl={item.icon}
                   title={item.title}
                   isActive={selectedSkill.title === item.title}
-                  onClick={() => handleSelectSkill(item)}
+                  onClick={() => {handleSelectSkill(item);}}
                 />
-                <ul>
-                  {item.Skills.map((skill, i) => (
-                    <li key={i}>
-                      {skill.skill}: {skill.Percentage}
-                    </li>
-                  ))}
-                </ul>
-              </div>
+              
             ))}
           </div>
         </div>
