f1b4bc9a0eb6ea35554573caba9b25dbf5a6b40c
Kanishka-Che <akcadikari@std.appsc.sab.ac.lk>
2024-10-02 15:53:26 +0530

    updated about Skills

--- portfoliosite/src/Components/Skills/SkillsinfoCard/SkillsinfoCard.jsx
+++ portfoliosite/src/Components/Skills/SkillsinfoCard/SkillsinfoCard.jsx
@@ -1,13 +1,27 @@
-import React from 'react'
-import './SkillsinfoCard.css'
+import React from 'react';
+import './SkillsinfoCard.css';
 
-
-const SkillsinfoCard = ({heading ,skills}) => {
+const SkillsinfoCard = ({ heading, skills }) => {
   return (
     <div className='skills-info-card'>
       <h6>{heading}</h6>
+
+      <div className='skills-info-content'>
+        {skills.map((item, index) => (
+          <React.Fragment key={`skill_${index}`}>
+            <div className='skill-info'>
+              <p>{item.skill}</p>
+              <p className='Percentage'>{item.Percentage}</p>
+            </div>
+
+            <div className='skill-progress-bg'>
+              <div className='skill-progress' style={{ width: item.Percentage }} />
+            </div> 
+          </React.Fragment>
+        ))}
+      </div>
     </div>
-  )
+  );
 };
 
-export default SkillsinfoCard
+export default SkillsinfoCard;
